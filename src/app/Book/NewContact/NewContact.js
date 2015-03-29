6c144cb43b6224de4f0dca4571c02521467a83c7
Vlada Janosevic <vladajanosevic@gmail.com>
2015-03-29 22:35:17 +0200

    New contact WIP

--- src/app/Book/NewContact/NewContact.js
+++ src/app/Book/NewContact/NewContact.js
@@ -3,7 +3,8 @@
 
 angular
 	.module('App.Book')
-	.controller('newContactCtrl', newContactCtrl);
+	.controller('newContactCtrl', newContactCtrl)
+	.directive ('autoAdd', autoAdd);
 
 
 //**************************************************************************************************
@@ -13,6 +14,54 @@ newContactCtrl.$inject = ['DataBase', '$scope'];
 function newContactCtrl( DataBase, $scope ) {
 	$scope.bookView.detailsOpen = true;
 	var vm = this;
-	vm.concat = {};
+	vm.contact = {
+		firstName:'',
+		middleName:'',
+		lastName:'',
+		nick:'',
+		address:'',
+		birthday:'',
+		phones:[],
+		emails:[],
+		links:[],
+		ims:[]
+	};
 }
+
+
+//**************************************************************************************************
+
+//autoAdd.$inject = ['$timeout'];
+
+function autoAdd() {
+	var root;
+
+	//--------------------------------------------------------------------------------------------------
+	function link(scope, ele, attrs) {
+		root = ele;
+		if(!(scope.collection instanceof Array)) { scope.collection = []; }
+		if(scope.collection.length===0) { scope.collection.push(['','']); }
+		console.log('autoAdd', scope.collection);
+	}
+/*
+	function getValidation() {
+		var c=this.contact,
+			n=this.newContactForm;
+		if(c.firstName==='' && c.middleName==='' && c.lastName==='' && c.nick==='') {return true;}
+		if(n.firstName.$invalid || n.middleName.$invalid || n.lastName.$invalid || n.nick.$invalid) {return true;}
+		return this.newContactForm.$invalid;
+	}*/
+
+
+	return {
+		scope:{
+			collection:'='
+		},
+		templateUrl:'autoAdd.tpl.html',
+		replace:true,
+		restrict:'A',
+		link:link
+	}
+}
+
 })();
