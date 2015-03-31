07097808e2c18c46ab32130f85167c3ddbce5da4
Vlada Janosevic <vladajanosevic@gmail.com>
2015-03-31 15:06:48 +0200

    New contact directives WIP

--- src/app/Book/NewContact/NewContact.js
+++ src/app/Book/NewContact/NewContact.js
@@ -4,7 +4,8 @@
 angular
 	.module('App.Book')
 	.controller('newContactCtrl', newContactCtrl)
-	.directive ('autoAdd', autoAdd);
+	.directive ('autoAdd', autoAdd)
+	.directive ('typeField', typeField);
 
 
 //**************************************************************************************************
@@ -21,7 +22,7 @@ function newContactCtrl( DataBase, $scope ) {
 		nick:'',
 		address:'',
 		birthday:'',
-		phones:[],
+		phones:[["999666","mob"],["666999","fix"]],
 		emails:[],
 		links:[],
 		ims:[]
@@ -41,23 +42,8 @@ function autoAdd() {
 		root = ele;
 		if(!(scope.collection instanceof Array)) { scope.collection = []; }
 		if(scope.collection.length===0) { scope.collection.push(['','']); }
-		scope.$watch('collection[0][0]', function(n, m) {
-			console.log('CHANGE', n, m);
-			scope.collection.forEach(valid);
-		});
-	}
 
-	function valid(e,i) {
-		console.log(i, e.$invalid);
 	}
-/*
-	function getValidation() {
-		var c=this.contact,
-			n=this.newContactForm;
-		if(c.firstName==='' && c.middleName==='' && c.lastName==='' && c.nick==='') {return true;}
-		if(n.firstName.$invalid || n.middleName.$invalid || n.lastName.$invalid || n.nick.$invalid) {return true;}
-		return this.newContactForm.$invalid;
-	}*/
 
 
 	return {
@@ -65,6 +51,47 @@ function autoAdd() {
 			collection:'='
 		},
 		templateUrl:'autoAdd.tpl.html',
+		replace:true,
+		restrict:'A',
+		link:link
+	}
+}
+
+
+//**************************************************************************************************
+
+//typeField.$inject = ['$timeout'];
+
+function typeField() {
+	var root;
+
+	//--------------------------------------------------------------------------------------------------
+	function link(scope, ele, attrs) {
+		root = ele;
+		console.log('typeField', scope.typeField);
+		scope.data = {
+			value: scope.typeField[0],
+			type: scope.typeField[1]
+		}
+		scope.$watch('data.value', function(n, m) {
+			console.log('CHANGE', n, m, ele.children()[1]);
+		});
+	}
+	/*
+	 function getValidation() {
+	 var c=this.contact,
+	 n=this.newContactForm;
+	 if(c.firstName==='' && c.middleName==='' && c.lastName==='' && c.nick==='') {return true;}
+	 if(n.firstName.$invalid || n.middleName.$invalid || n.lastName.$invalid || n.nick.$invalid) {return true;}
+	 return this.newContactForm.$invalid;
+	 }*/
+
+
+	return {
+		scope:{
+			typeField:'='
+		},
+		templateUrl:'typeField.tpl.html',
 		replace:true,
 		restrict:'A',
 		link:link
