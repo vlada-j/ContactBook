efebf2d9afdda54b6755fe4bdcffee76c3b3e271
Vlada Janosevic <vladajanosevic@gmail.com>
2015-04-09 21:15:42 +0200

    Move contact directives to Contact modul

--- src/app/Book/Contact/Contact.js
+++ src/app/Book/Contact/Contact.js
@@ -2,8 +2,88 @@
 'use strict';
 
 angular
-	.module('App.Book.Contact', []);
-	//.directive ('autoAdd', autoAdd)
-	//.directive ('typeField', typeField);
+	.module('App.Book.Contact', [])
+	.directive ('autoAdd', autoAdd)
+	.directive ('typeField', typeField);
+
+
+//**************************************************************************************************
+function autoAdd() {
+
+	//--------------------------------------------------------------------------------------------------
+	function link(scope, ele, attrs) {
+		var root = ele;
+		var col = scope.collection;
+		if(!(col instanceof Array)) { col = []; }
+		if(col.length===0) { col.push(['','']); }
+
+		scope.empty = function(it) {
+			if(col.length>0 && it.length===2) {
+				update(col, it);
+			} else {
+				console.log('Error:', col, it);
+			}
+		};
+
+		update(col);
+	}
+
+	function update(c, i) {
+		var l = c[c.length-1];
+		if(l[0] !== undefined && l[0] !== '') {
+			c.push(['', '']);
+		}
+		if(i && !i[0]) {
+			c.splice(c.indexOf(i), 1);
+		}
+	}
+
+	return {
+		scope:{
+			collection:'='
+		},
+		templateUrl:'autoAdd.tpl.html',
+		restrict:'A',
+		link:link
+	}
+}
+
+
+//**************************************************************************************************
+function typeField() {
+	var root;
+
+	//--------------------------------------------------------------------------------------------------
+	function link(scope, ele, attrs) {
+		root = ele;
+		scope.data = scope.typeField;
+
+		scope.$watch('data[0]', function(n, o) {
+			if(n !== o) {
+				scope.typeFieldCallback(scope.typeField);
+			}
+		});
+	}
+	/*
+	 function getValidation() {
+	 var c=this.contact,
+	 n=this.newContactForm;
+	 if(c.firstName==='' && c.middleName==='' && c.lastName==='' && c.nick==='') {return true;}
+	 if(n.firstName.$invalid || n.middleName.$invalid || n.lastName.$invalid || n.nick.$invalid) {return true;}
+	 return this.newContactForm.$invalid;
+	 }*/
+
+
+	return {
+		scope:{
+			typeField:'=',
+			typeFieldCallback:'='
+		},
+		templateUrl:'typeField.tpl.html',
+		replace:true,
+		restrict:'A',
+		link:link
+	}
+}
 
 })();
--- src/app/Book/NewContact/NewContact.js
+++ src/app/Book/NewContact/NewContact.js
@@ -3,9 +3,7 @@
 
 angular
 	.module('App.Book')
-	.controller('newContactCtrl', newContactCtrl)
-	.directive ('autoAdd', autoAdd)
-	.directive ('typeField', typeField);
+	.controller('newContactCtrl', newContactCtrl);
 
 
 //**************************************************************************************************
@@ -27,92 +25,6 @@ function newContactCtrl( DataBase, $scope ) {
 		links:[],
 		ims:[]
 	};
-}
-
-
-//**************************************************************************************************
-
-//autoAdd.$inject = ['$timeout'];
-
-function autoAdd() {
-
-	//--------------------------------------------------------------------------------------------------
-	function link(scope, ele, attrs) {
-		var root = ele;
-		var col = scope.collection;
-		if(!(col instanceof Array)) { col = []; }
-		if(col.length===0) { col.push(['','']); }
-
-		scope.empty = function(it) {
-			if(col.length>0 && it.length===2) {
-				update(col, it);
-			} else {
-				console.log('Error:', col, it);
-			}
-		};
-
-		update(col);
-	}
-
-	function update(c, i) {
-		var l = c[c.length-1];
-		if(l[0] !== undefined && l[0] !== '') {
-			c.push(['', '']);
-		}
-		if(i && !i[0]) {
-			c.splice(c.indexOf(i), 1);
-		}
-	}
-
-	return {
-		scope:{
-			collection:'='
-		},
-		templateUrl:'autoAdd.tpl.html',
-		restrict:'A',
-		link:link
-	}
-}
-
-
-//**************************************************************************************************
-
-//typeField.$inject = ['$timeout'];
-
-function typeField() {
-	var root;
-
-	//--------------------------------------------------------------------------------------------------
-	function link(scope, ele, attrs) {
-		root = ele;
-		scope.data = scope.typeField;
-
-		scope.$watch('data[0]', function(n, o) {
-			if(n !== o) {
-				scope.typeFieldCallback(scope.typeField);
-			}
-		});
-	}
-	/*
-	 function getValidation() {
-	 var c=this.contact,
-	 n=this.newContactForm;
-	 if(c.firstName==='' && c.middleName==='' && c.lastName==='' && c.nick==='') {return true;}
-	 if(n.firstName.$invalid || n.middleName.$invalid || n.lastName.$invalid || n.nick.$invalid) {return true;}
-	 return this.newContactForm.$invalid;
-	 }*/
-
-
-	return {
-		scope:{
-			typeField:'=',
-			typeFieldCallback:'='
-		},
-		templateUrl:'typeField.tpl.html',
-		replace:true,
-		restrict:'A',
-		link:link
-	}
 }
 
 })();
